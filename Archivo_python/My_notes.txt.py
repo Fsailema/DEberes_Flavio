402b4b8ae9cb22290d2c13bbd59b10a51ac25c80
Fsailema <142652836+Fsailema@users.noreply.github.com>
2023-10-01 21:10:53 -0500

    Tareas

--- Archivo_python/My_notes.txt.py
+++ Archivo_python/My_notes.txt.py
@@ -1,22 +1,31 @@
-
+#creamos el archivo
 direccion = "my_notes.txt"
+
+#abrimos el archivo
 file = open(direccion, 'r')
 print(file)
 
 print("___________________________________________")
-
-lineas = file.readlines()
+#para leer el archivo linea por linea
+lineas = file.readline()
+print(lineas)
+lineas = file.readline()
+print(lineas)
+lineas = file.readline()
 print(lineas)
 
-for l in lineas:
-    print(l.replace('\n', ''))
-
 file.close()
+
+#para leer como una lista
 
 with open('my_notes.txt', 'r') as archivo:
     argumento = archivo.read()
     lineas = argumento.split('\n')
     print(lineas)
 
+#para leer todo sin el espacio
+
+for l in lineas:
+    print(l.replace('\n', ''))
 
 
